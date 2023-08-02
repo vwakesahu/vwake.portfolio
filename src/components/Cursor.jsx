886c0a3da5c61193a47a1c43e3684b0a21b3a0ce
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-08-02 23:15:30 +0530

    Changing Cursor Animations

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -1,50 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import './Cursor.css'
-import { motion } from "framer-motion";
-
+import React, { useEffect, useState } from 'react';
+import './Cursor.css';
+import { motion } from 'framer-motion';
 
 const Cursor = ({ scaling }) => {
+  const [outerCircle, setOuterCircle] = useState({ x: -45, y: -45 });
+  const [innerCircle, setInnerCircle] = useState({ x: -45, y: -45 });
 
-    const [outterCircle, setoutterCircle] = useState({ x: -45, y: -45 });
-    const [innerCircle, setinnerCircle] = useState({ x: -45, y: -45 });
+  useEffect(() => {
+    const mousemove = (e) => {
+      setOuterCircle({ x: e.clientX, y: e.clientY });
+      setInnerCircle({ x: e.clientX, y: e.clientY });
+    };
+    window.addEventListener('mousemove', mousemove);
 
-    useEffect(() => {
-        const mousemove = (e) => {
-            setoutterCircle({ x: e.clientX, y: e.clientY });
-            setinnerCircle({ x: e.clientX, y: e.clientY });
-        };
-        window.addEventListener("mousemove", mousemove);
+    return () => {
+      window.removeEventListener('mousemove', mousemove);
+    };
+  }, []);
 
-        return () => {
-            window.removeEventListener("mousemove", mousemove);
-        };
-    }, []);
+  return (
+    <div className='hidden bg-transparent md:flex absolute items-center justify-center'>
+      <motion.div
+        animate={{
+          x: innerCircle.x,
+          y: innerCircle.y,
+          transition: { type: 'spring', stiffness: 1000000, mass: 0.01 },
+        }}
+        className='outer_circle'
+      ></motion.div>
+      <motion.div
+        animate={{
+          x: outerCircle.x,
+          y: outerCircle.y,
+        }}
+        className='inner_circle absolute'
+        style={{ scale: scaling ? 0.1 : 1 }}
+      ></motion.div>
+    </div>
+  );
+};
 
-
-    return (
-        <div className='hidden bg-transparent md:flex absolute items-center justify-center'>
-            
-            <motion.div
-                animate={{
-                    x: innerCircle.x,
-                    y: innerCircle.y,
-                    transition: { type: "spring", stiffness:1000000, mass: 0.01 },
-
-                }} 
-                className='outer_circle'
-                >
-            </motion.div>
-            <motion.div
-                animate={{
-                    x: outterCircle.x,
-                    y: outterCircle.y,
-                    transition: { type: "spring", stiffness:100, mass: 0.07 },
-                }} 
-                className='inner_circle absolute '   
-                style={{ scale: scaling ? 0.1 : 1 }}>
-            </motion.div>
-        </div>
-    )
-}
-
-export default Cursor
+export default Cursor;
