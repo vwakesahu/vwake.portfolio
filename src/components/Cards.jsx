7acd86fff1d84cbfa7bad8f56835e0e6b385181a
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-08-26 23:49:50 +0530

    Added Web D Projects

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Cards = ({ imageSrc, title, description, buttonText, bgColor}) => {
   return (
-    <div className="md:mt-36 border rounded-lg md:w-[25%] p-4">
+    <div className=" border rounded-lg md:w-[25%] p-4">
       <img src={imageSrc} className="w-full h-40 md:h-60 " alt="" />
       <div className="mt-6 flex flex-col">
         <p className="text-xl font-semibold">{title}</p>
