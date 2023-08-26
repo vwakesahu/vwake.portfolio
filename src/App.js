87e84ff57051e3aa7e08bb717a8a0904ebe4526c
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-08-26 23:12:36 +0530

    Added Card Data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Cards from "./components/Cards";
 import Cursor from "./components/Cursor";
 import HeroSection from "./components/HeroSection";
 import FoodApp from "./img/food_app.png";
+import JobSet from "./img/jobset.png"
+import BullishBrains from "./img/builish_brains.png"
 
 function App() {
   const cardDataArray = [
@@ -14,18 +16,18 @@ function App() {
       bgColor:"bg-accYellow"
     },
     {
-      imageSrc: FoodApp,
-      title: "Canteen",
+      imageSrc: JobSet,
+      title: "JobSet",
       description:
-      "Canteen is a cutting-edge Online Food Ordering System created using ReactJS and enhanced with Tailwind CSS for seamless and appealing user interfaces. With the integration of Firebase, the platform utilizes Google authentication for a secure and user-friendly login process. The application effectively employs a Redux-powered reducer store to manage the shopping cart, ensuring a smooth and interactive experience for users as they curate and confirm their orders. By harmonizing these technologies, Canteen facilitates a streamlined user journey from browsing the menu to placing orders, contributing to an efficient and satisfying food ordering process.",
+      "During my tenure as an intern in the IBM Skillbuild program for web development, I actively participated in a collaborative project called JobSet. This project focused on creating a comprehensive web application to facilitate job-related activities. My role involved working on various key components of the application, including the Homepage, Resources section, and the Find Job feature. I employed ReactJS and Tailwind CSS, two powerful technologies, to develop these components. ReactJS enabled me to build dynamic and responsive user interfaces, while Tailwind CSS streamlined the design process and ensured a visually cohesive presentation. Through this experience, I not only refined my technical skills but also gained valuable insights into effective collaboration. By contributing to different facets of the JobSet project, I learned how to work harmoniously within a team, manage code integration, and maintain consistent coding practices. Overall, my involvement in JobSet allowed me to apply my knowledge of ReactJS and Tailwind CSS to a real-world project, while also fostering a deeper understanding of teamwork and collaborative software development.",
       buttonText: "Read Workflow",
       bgColor:"bg-accPurple"
     },
     {
-      imageSrc: FoodApp,
-      title: "Canteen",
+      imageSrc: BullishBrains,
+      title: "Bullish Brains",
       description:
-        "Canteen is a cutting-edge Online Food Ordering System created using ReactJS and enhanced with Tailwind CSS for seamless and appealing user interfaces. With the integration of Firebase, the platform utilizes Google authentication for a secure and user-friendly login process. The application effectively employs a Redux-powered reducer store to manage the shopping cart, ensuring a smooth and interactive experience for users as they curate and confirm their orders. By harmonizing these technologies, Canteen facilitates a streamlined user journey from browsing the menu to placing orders, contributing to an efficient and satisfying food ordering process.",
+        "In my portfolio, I also had the privilege of contributing to a project called Bullish Brains, a dynamic platform centered around the stock market. This project catered to stock market enthusiasts by providing real-time news updates and interactive graphical representations of stock data obtained through APIs. Incorporating a range of functionalities, Bullish Brains seamlessly displayed live news updates, ensuring users were always informed about the latest developments in the stock market. Additionally, the platform offered the capability to visualize stock data through dynamic graphs, helping users analyze market trends and make informed decisions. For user authentication, the project implemented a secure and user-friendly approach. It leveraged email verification and Google sign-in, utilizing the authentication services offered by Firebase. This not only ensured a robust login system but also contributed to a seamless and convenient user experience. By harmonizing these elements, Bullish Brains emerged as a comprehensive tool that empowered users to stay updated on stock market happenings, make data-driven investment choices, and engage with a user interface that combined real-time information and intuitive design.",
       buttonText: "Read Workflow",
       bgColor:"bg-accBlue"
     },
